6c60d092d9b55428692add65db2f23b118338021
Bikram@Lohar <bikramlohar2345@gmail.com>
2025-08-19 11:27:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/', function (req, res) {
+    res.send("Hello World")
+})
+
+app.get('/about', (req, res) => {
+    res.send("it is a about page")
+})
+
+app.get('/github', (req, res) => {
+    res.send("This is my github page");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Your Backend Is Running")
+})
