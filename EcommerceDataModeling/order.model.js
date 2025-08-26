9ded512ded64c58ab8862dd1c3ab923842825173
Bikram@Lohar <bikramlohar2345@gmail.com>
2025-08-26 15:15:59 +0530

    oderModel Updated

--- EcommerceDataModeling/order.model.js
+++ EcommerceDataModeling/order.model.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-    orderName: {
-        type: String,
+    orderPrice: {
+        type: Number,
         required: true
-    }
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    orderItems:{
+
+    } 
 }, { timestamps: true })
 
 export const Order = mongoose.model("Order", orderSchema)
