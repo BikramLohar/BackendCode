b2f2212935693751c2c1ece78633fa3ff7f87449
Bikram@Lohar <bikramlohar2345@gmail.com>
2025-08-28 15:10:38 +0530

    newModels added

---
+++ HospitalManagementData/appointment.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const appointmentSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient",
+        required: true
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Scheduled", "Completed", "Cancelled"],
+        default: "Scheduled"
+    }
+}, { timestamps: true })
+
+export const Appointment = mongoose.model("Appointment", appointmentSchema)
+
---
+++ HospitalManagementData/bill.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const billSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient"
+    },
+    treatmentCost: {
+        type: Number
+    },
+    roomCost: { type: Number },
+    medicineCost: Number,
+    totalAmount: Number,
+    paymentStatus: {
+        type: String,
+        enum: ["Pending", "Paid"],
+        default: "Pending"
+    }
+}, { timestamps: true })
+
+export const Bill = mongoose.model("Bill", billSchema)
